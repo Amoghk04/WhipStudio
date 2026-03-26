e070fbdf2c14490a8165805d52abf4b2245e6a7a
Amoghk04 <kalasapuraamogh@gmail.com>
2026-03-26 14:18:59 +0530

    WhipStudio: Initial code

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Whipstudio environment server components."""
+
+from .WhipStudio_environment import WhipstudioEnvironment
+
+__all__ = ["WhipstudioEnvironment"]
